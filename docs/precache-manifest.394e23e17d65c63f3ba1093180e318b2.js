45d67069dd4ea0847dbec50c8fb5f2985d09c573
Marianissimus <marianissimus@gmail.com>
2020-03-20 16:57:54 +0200

    upload dist folder as docs for deploy

---
+++ docs/precache-manifest.394e23e17d65c63f3ba1093180e318b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0790d482ee80858f7845",
+    "url": "/ordergame/css/app.dca891b8.css"
+  },
+  {
+    "revision": "0790d482ee80858f7845",
+    "url": "/ordergame/js/app.752a89ee.js"
+  },
+  {
+    "revision": "e9dd60b1e32d54e08c46",
+    "url": "/ordergame/js/chunk-vendors.649ec645.js"
+  },
+  {
+    "revision": "36376d67eb65b4ee31fb7633e2d66d8d",
+    "url": "/ordergame/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ordergame/robots.txt"
+  }
+];
